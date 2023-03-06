fc23766f6d7c6100cc86a159387ab30cb3b78725
SachenkoTanya <tanasacenko416@gmail.com>
2023-03-06 20:44:30 +0200

    Task 1 complete

---
+++ Task 1/task1.py
@@ -0,0 +1,13 @@
+print("Multiparadigm programming languages,  Task1")
+print("Sachenko Tanya,  IKM-221a")
+
+print(" ")
+
+for i in range(1, 45):
+    print("Владислав Валерійович Овсяніков,", end=" ")
+print("Владислав Валерійович Овсяніков")
+
+print(" ")
+
+result = (113.2 / (21.2 + 11.3)) + (22.2 / (15.2 - 11.3))
+print("result =", result)
