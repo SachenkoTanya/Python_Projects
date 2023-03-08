3ae76fc54d3e3f0d3afa3ab4f7df38a5fefd3468
SachenkoTanya <tanasacenko416@gmail.com>
2023-03-08 19:21:57 +0200

    refactor lab_from_c_plus_plus

--- LabRewritted/LabFromC++.py
+++ LabRewritted/LabFromC++.py
@@ -1,25 +1,25 @@
 import random
 
-def fill_arr(arr, M, N):
+def fill_arr(arr, rows, cols):
     random.seed()
-    for i in range(M):
-        for j in range(N):
+    for i in range(rows):
+        for j in range(cols):
             arr[i][j] = random.randint(0, 9)
 
 
-def print_arr(arr, M, N):
-    for i in range(M):
-        for j in range(N):
+def print_arr(arr, rows, cols):
+    for i in range(rows):
+        for j in range(cols):
             print(arr[i][j], end=" ")
         print()
 
 
-def seek_min_elem(arr, M, N):
+def seek_min_elem(arr, rows, cols):
     min_val = float('inf')
     min_i = 0
 
-    for i in range(M):
-        for j in range(N):
+    for i in range(rows):
+        for j in range(cols):
             if arr[i][j] < min_val:
                 min_val = arr[i][j]
                 min_i = i
@@ -27,26 +27,23 @@ def seek_min_elem(arr, M, N):
     print("\nMIN:", min_val)
     print("MIN_i:", min_i, "\n")
 
-    M, N, new_arr = cut_arr(arr, M, N, min_i)
-    return M, N, new_arr
+    rows, cols, new_arr = cut_arr(arr, rows, cols, min_i)
+    return rows,cols, new_arr
 
 
-def cut_arr(arr, M, N, min_i):
-    new_arr = []
-    for i in range(M):
-        if i != min_i:
-            new_arr.append(arr[i])
-    M -= 1
-    return M, N, new_arr
+def cut_arr(arr, rows, cols, min_i):
+    new_arr = [x for x in arr if x != arr[min_i]]
+    rows -= 1
+    return rows, cols, new_arr
 
 
 if __name__ == "__main__":
-    M = int(input("M: "))
-    N = int(input("N: "))
+    rows = int(input("Rows: "))
+    cols = int(input("Cols: "))
 
-    arr = [[0 for j in range(N)] for i in range(M)]
+    arr = [[0 for j in range(cols)] for i in range(rows)]
 
-    fill_arr(arr, M, N)
-    print_arr(arr, M, N)
-    M, N, arr = seek_min_elem(arr, M, N)
-    print_arr(arr, M, N)
+    fill_arr(arr, rows, cols)
+    print_arr(arr, rows, cols)
+    rows, cols, arr = seek_min_elem(arr, rows, cols)
+    print_arr(arr, rows, cols)
