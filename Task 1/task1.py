aa28eea28273423d704c5ed000b95f81051da632
SachenkoTanya <tanasacenko416@gmail.com>
2023-03-08 18:58:23 +0200

    refactor lab1

--- Task 1/task1.py
+++ Task 1/task1.py
@@ -1,13 +1,10 @@
-print("Multiparadigm programming languages,  Task1")
-print("Sachenko Tanya,  IKM-221a")
+print('Multiparadigm programming languages,  Task1')
+print('Sachenko Tanya,  IKM-221a', end='\n\n')
 
-print(" ")
+arr = ['Vladislav Valerievich Ovsyannikov'] * 45
+print(', '.join(arr))
 
-for i in range(1, 45):
-    print("Владислав Валерійович Овсяніков,", end=" ")
-print("Владислав Валерійович Овсяніков")
-
-print(" ")
+print('\n')
 
 result = (113.2 / (21.2 + 11.3)) + (22.2 / (15.2 - 11.3))
-print("result =", result)
+print('result = ', result)
