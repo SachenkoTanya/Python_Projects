300a6dd87a8626974d66a35eb81f74495551b059
SachenkoTanya <tanasacenko416@gmail.com>
2023-03-09 17:46:03 +0200

    Done task 2

---
+++ Task2.py
@@ -0,0 +1,15 @@
+#Multiparadigm programming languages,  Task2
+#Sachenko Tanya,  IKM-221a
+
+print('Multiparadigm programming languages,  Task2')
+print('Sachenko Tanya,  IKM-221a', end='\n\n')
+
+x = float(input('Input  x  please: '))
+y = float(input('Input  y  please: '))
+z = float(input('Input  z  please: '))
+
+if x == -11.3 or z == 11.3:
+    print('Error!!!')
+else:
+    print((x / (x + 11.3)) + (y / (z - 11.3)))
+
