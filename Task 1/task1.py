b9176383f6c6c6911b0f6d9f215948cc4ed0c663
SachenkoTanya <tanasacenko416@gmail.com>
2023-03-13 08:51:41 +0200

    Redo the laboratory 1

--- Task 1/task1.py
+++ Task 1/task1.py
@@ -1,5 +1,5 @@
-print('Multiparadigm programming languages,  Task1')
-print('Sachenko Tanya,  IKM-221a', end='\n\n')
+print('Multiparadigm programming languages, Task1')
+print('Sachenko Tanya, IKM-221a', end='\n\n')
 
 arr = ['Vladislav Valerievich Ovsyannikov'] * 45
 print(', '.join(arr))
@@ -7,4 +7,4 @@ print(', '.join(arr))
 print('\n')
 
 result = (113.2 / (21.2 + 11.3)) + (22.2 / (15.2 - 11.3))
-print('result = ', result)
+print(f'result = {result}')
