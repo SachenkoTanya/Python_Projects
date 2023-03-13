f894418c982304cb069cda1f6cc61c776e40c8c7
SachenkoTanya <tanasacenko416@gmail.com>
2023-03-13 23:19:27 +0200

    rewritted task3

--- Task3.py
+++ Task3.py
@@ -1,22 +1,22 @@
 # Multiparadigm programming languages,  Task2
 # Sachenko Tanya,  IKM-221a
 
+import math
+
 print('Multiparadigm programming languages,  Task2')
 print('Sachenko Tanya,  IKM-221a', end='\n\n')
 
-import math
-
 try:
-    a = float(input("Enter value for a: "))
-    b = float(input("Enter value for b: "))
+    a = float(input('Enter value for a: '))
+    b = float(input('Enter value for b: '))
 
     if b == a:
-        raise ValueError("Error: b and a cannot have the same value")
+        raise ValueError('Error: b and a cannot have the same value')
     if b > a:
-        raise ValueError("Error: b cannot be greater than a")
+        raise ValueError('Error: b cannot be greater than a')
 
     result = math.sqrt((a - b) ** 5 / (b - a) ** 3)
-    print(f"Result: {result}")
+    print(f'Result: {result}')
 
 except ValueError as b_error:
-    print("Error with b value:", b_error)
+    print('Error with b value:', b_error)
