6abfa6aa73550862d7423863bf901bdba3833533
SachenkoTanya <tanasacenko416@gmail.com>
2023-03-23 09:59:34 +0200

    Refactor lab4

--- Task4.py
+++ Task4.py
@@ -1,9 +1,6 @@
 # Multiparadigm programming languages,  Task2
 # Sachenko Tanya,  IKM-221a
 
-print('Multiparadigm programming languages,  Task2')
-print('Sachenko Tanya,  IKM-221a', end='\n\n')
-
 
 def is_prime(max_value):
     if max_value <= 1:
@@ -13,10 +10,19 @@ def is_prime(max_value):
             return False
     return True
 
+max_value = 0
+while True:
+    try:
+        max_value = int(input('Enter the maximum value to check: '))
+        break
+    except ValueError:
+        print('Must be number!!!')
 
-max_value = int(input("Enter the maximum value to check: "))
 
 for current_number in range(2, max_value + 1):
     mersenne_number = 2 ** current_number - 1
     if is_prime(current_number) and is_prime(mersenne_number):
         print(mersenne_number)
+
+print('Multiparadigm programming languages,  Task2')
+print('Sachenko Tanya,  IKM-221a', end='\n\n')
