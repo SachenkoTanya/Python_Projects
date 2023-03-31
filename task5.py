44928b57020a756153c01b681ffe3cdc87e2cc28
SachenkoTanya <tanasacenko416@gmail.com>
2023-03-31 14:36:18 +0300

    Refactor lab5

--- task5.py
+++ task5.py
@@ -1,43 +1,40 @@
 import math
 
 
+def get_number(message, expected_type=float):
+    user_input = input(message)
+    try:
+        converted_input = expected_type(user_input)
+        return converted_input
+    except ValueError:
+        print(f'The value must be of type {expected_type.__name__}')
+        return get_number(message, expected_type)
+
+
 # 1 part
 
 # Initialize variables
-N = 1
-SUM = 0
+n = 1
+row_sum = 0
 
 # Calculation of the sum of the first 13 members of the series
-while N <= 13:
-    an = math.log(math.factorial(N)) / (N ** 2)
-    SUM += an
-    N += 1
+while n <= 13:
+    an = math.log(math.factorial(n)) / (n ** 2)
+    row_sum += an
+    n += 1
 
 # Display the result
-print('The sum of the first 13 members of the series: ', SUM)
-
+print('The sum of the first 13 members of the series: ', row_sum)
 
 # 2 part
-while True:
-    try:
-        N = int(input('Enter an integer: '))
-        count = int(math.log10(N)) + 1
-        print('The number of digits in the entered number:', count)
-        break
-    except ValueError:
-        print('You entered the wrong value!')
-
+n = get_number('Enter an integer: ', int)
+count = int(math.log10(n)) + 1
+print('The number of digits in the entered number:', count)
 
 # 3 part
-while True:
-    try:
-        N = float(input('Enter a number: '))
-    except ValueError:
-        print('You entered the wrong value!')
-    else:
-        x = N
-        E = 1e-4
-        while abs(x * x - N) > E:
-            x = (x + N / x) / 2
-        print('The square root of a number', N, 'is equal to', x)
-        break
+n = get_number('Enter a number: ', float)
+x = n
+E = 1e-4
+while abs(x * x - n) > E:
+    x = (x + n / x) / 2
+print('The square root of a number', n, 'is equal to', x)
